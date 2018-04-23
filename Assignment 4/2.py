6e9c6eb810cf2cbc61482e452909137f95992bb5
Aaditya <DrakeAaditya@users.noreply.github.com>
2018-04-23 09:20:56 +0530

    Retrieval from supplier with condition

--- Assignment 4/2.py
+++ Assignment 4/2.py
@@ -1,1 +1,10 @@
-3
+import mysql.connector;
+connection = mysql.connector.connect(host='localhost',database='rdbms',user='root',password='aaditya')
+cur = connection.cursor();
+sql = """SELECT suppliername, suppliercontactno FROM supplier WHERE supplierid = 'S1002';"""
+cur.execute(sql)
+data = cur.fetchall();
+for row in data:
+    print(row)
+connection.commit()
+connection.close()
