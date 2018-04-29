58334cff806e33979d0e252931f49dcdff8c9079
Aaditya <DrakeAaditya@users.noreply.github.com>
2018-04-29 13:23:40 +0530

    Update 1.py

--- Assignment 5/1.py
+++ Assignment 5/1.py
@@ -1,1 +1,12 @@
-#
+import mysql.connector
+connection = mysql.connector.connect(host='localhost',database='python_mysql',user='root',password='aaditya')
+cur = connection.cursor();
+supplierId = input("Enter Supplier ID > ")
+supplierName = input("Enter Supplier Name > ")
+suppliercontactno = input("Enter Supplier Contact No. > ")
+supplieremailid = input("Enter Supplier Email Id > ")
+
+cur.execute("""INSERT INTO supplier VALUES
+                (%s,%s,%s,%s)""",(supplierId,supplierName,suppliercontactno,supplieremailid))
+connection.commit()
+connection.close()
