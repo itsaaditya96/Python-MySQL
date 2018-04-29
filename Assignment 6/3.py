a1a2763da22c02b2a6524903448eb268d2b70625
Aaditya <DrakeAaditya@users.noreply.github.com>
2018-04-29 14:32:27 +0530

    Update 3.py

--- Assignment 6/3.py
+++ Assignment 6/3.py
@@ -1,5 +1,5 @@
 import mysql.connector
-connection = mysql.connector.connect(host='localhost',database='rdbms',user='root',password='aaditya')
+connection = mysql.connector.connect(host='localhost',database='python_mysql',user='root',password='aaditya')
 cur = connection.cursor();
 sql = """DELETE FROM supplier
         where supplierid='S1009'"""
