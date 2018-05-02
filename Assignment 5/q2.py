89d05d8ec94da232e846a9dd36c1c0ef47d632d7
Riya Nagpal <riyanagpal1009@gmail.com>
2018-05-02 23:20:46 +0530

    Rename 2.py to q2.py

---
+++ Assignment 5/q2.py
@@ -0,0 +1,14 @@
+import mysql.connector;
+con = mysql.connector.connect(host ="localhost", user= "root",database="py",password="2429")
+cur = con.cursor()
+supplierId = input("Enter supplier Id> ")
+supplierName = input("Enter supplier Name> ")
+suppliercontactno = input("Enter supplier Contac No> ")
+supplieremailId = input("Enter supplier Email Id> ")
+
+bindVar = {'paramsupid':supplierId,'paramsupname':supplierName,'paramsupcontactno':suppliercontactno,'paramemail':supplieremailId}
+
+cur.execute("INSERT INTO supplier VALUES(%(paramsupid)s,%(paramsupname)s,%(paramsupcontactno)s,%(paramemail)s)""",bindVar)
+
+con.commit()
+con.close()
