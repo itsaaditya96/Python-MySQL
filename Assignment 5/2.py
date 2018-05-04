a18fe472d8cbf7662162f0731f13715425257ab9
Aaditya <DrakeAaditya@users.noreply.github.com>
2018-05-04 22:37:16 +0530

    Converted according to my database

--- Assignment 5/2.py
+++ Assignment 5/2.py
@@ -1,14 +1,12 @@
-+import mysql.connector;
-+con = mysql.connector.connect(host ="localhost", user= "root",database="py",password="2429")
-+cur = con.cursor()
-+supplierId = input("Enter supplier Id> ")
-+supplierName = input("Enter supplier Name> ")
-+suppliercontactno = input("Enter supplier Contac No> ")
-+supplieremailId = input("Enter supplier Email Id> ")
-+
-+bindVar = {'paramsupid':supplierId,'paramsupname':supplierName,'paramsupcontactno':suppliercontactno,'paramemail':supplieremailId}
-+
-+cur.execute("INSERT INTO supplier VALUES(%(paramsupid)s,%(paramsupname)s,%(paramsupcontactno)s,%(paramemail)s)""",bindVar)
-+
-+con.commit()
-+con.close()
+import mysql.connector
+connection = mysql.connector.connect(host='localhost',database='python_mysql',user='root',password='aaditya')
+cur = connection.cursor();
+supplierId = input("Enter Supplier ID > ")
+supplierName = input("Enter Supplier Name > ")
+suppliercontactno = input("Enter Supplier Contact No. > ")
+supplieremailid = input("Enter Supplier Email Id > ")
+bindVar={'paramsupID':supplierId,'paramsupName':supplierName,'paramsupcontact':suppliercontactno,'paramsupemail':supplieremailid}
+print(bindVar.get('paramsupID'))
+cur.execute("""INSERT INTO supplier VALUES(%(paramsupID)s,%(paramsupName)s,%(paramsupcontact)s,%(paramsupemail)s)""",bindVar)
+connection.commit()
+connection.close()
